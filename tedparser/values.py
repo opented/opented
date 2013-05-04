fa8edb5fa10e7ab774053b8e0ada706aefd94fa5
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-04 11:50:07 +0200

    more cases, relax regex.

--- tedparser/values.py
+++ tedparser/values.py
@@ -1,7 +1,7 @@
 import re
 from pprint import pprint
 
-NUMRE = '\d[\. \d]*(,\d{1,2})?'
+NUMRE = '(-|\s{0,5}|\d[\. \d]*(,\d{1,2})?)'
 CURRE = '(HT )?(?P<cur>[A-Z]{3})'
 
 VALUE_LINE_RE = re.compile('^(Value|Amount|Value of the contract):?\s*(?P<val>' + \
@@ -146,7 +146,49 @@ if __name__ == '__main__':
         ['Value: 1 342 685,90 PLN', 'Including VAT. VAT rate (%) 23'],
         ['Value 732 758,62 EUR'],
         ['Value 5 404 552,35 EUR', 'Excluding VAT'],
-        ['Lowest offer 35 536,45 and highest offer 323 753,45 EUR', 'Including VAT. VAT rate (%) 19']
+        ['Lowest offer 35 536,45 and highest offer 323 753,45 EUR', 'Including VAT. VAT rate (%) 19'],
+        ['Lowest offer 293 818 and highest offer  EUR', 'Excluding VAT'],
+        ['Lowest offer 1 993 260,69 and highest offer  PLN', 'Including VAT. VAT rate (%) 7.0'],
+        ['Value 0,0439 EUR', 'Excluding VAT'],
+        ['Value 260 841,985 EUR', 'Excluding VAT'],
+        ['Lowest offer 5 619 851,28 and highest offer  EUR', 'Including VAT. VAT rate (%) 19.0'],
+        ['Lowest offer  and highest offer 1 882 483 EUR', 'Excluding VAT'],
+        ['Value rd. 447 000 EUR', 'Excluding VAT'],
+        ['Value 50 000 000 - 2 000 000 000 HUF', 'Excluding VAT'],
+        ['Lowest offer 348 583,99 and highest offer  PLN', 'Including VAT. VAT rate (%) 22.0'],
+        ['Lowest offer  and highest offer 800 400 EUR', 'Including VAT. VAT rate (%) 16.0'],
+        ['Value  EUR', 'Excluding VAT'],
+        ['Value 200 000 000 - 300 000 000 HUF', 'Excluding VAT'],
+        ['Value 4,352 EUR', 'Including VAT. VAT rate (%)'],
+        ['Lowest offer  and highest offer 597 835,79 EUR'],
+        ['Value Importo stimato a corpo quale corrispettivo della prestazione complessiva a base di gara: 284 370,13 EUR (diconsi duecentoottantaquattromilatrecentosettanta/13) inclusi IVA ed oneri di legge EUR'],
+        ['Lowest offer  and highest offer 6 414 183,82 EUR', 'Excluding VAT'],
+        ['Value 245 586 (36 Monate) EUR', 'Including VAT. VAT rate (%) 19'],
+        ['Value 7 440 000 = lordo EUR'],
+        ['Lowest offer  and highest offer 143 103,45 EUR', 'Excluding VAT'],
+        ['Lowest offer 1 113 794,42 and highest offer  EUR', 'Including VAT. VAT rate (%) 19'],
+        ['Value 3 021 Teuro EUR', 'Excluding VAT'],
+        ['Lowest offer 220 000 and highest offer  EUR', 'Excluding VAT'],
+        [u'Lowest offer 1 228 547,98 Euro j\xe4hrlich bei einer Vertragsdauer von 4 Jahren and highest offer  EUR'],
+        ['Lowest offer 158 798,13 and highest offer  EUR', 'Excluding VAT'],
+        ['Lowest offer  and highest offer 466 831,08 EUR', 'Excluding VAT'],
+        ['Value 355,000 GBP', 'Excluding VAT'],
+        ['Lowest offer 76 041,40 and highest offer  EUR', 'Including VAT. VAT rate (%) 19'],
+        ['Lowest offer 74 215,86 and highest offer  EUR', 'Including VAT. VAT rate (%) 19'],
+        ['Lowest offer 158 798,13 and highest offer  EUR', 'Excluding VAT'],
+        ['Value 6 000 000 (BA I - III) EUR', 'Excluding VAT'],
+        ['Lowest offer 237 405 and highest offer  EUR', 'Including VAT. VAT rate (%) 19'],
+        ['Value 594 000 (importo lordo) EUR'],
+        ['Lowest offer 573 400 and highest offer  EUR', 'Excluding VAT'],
+        ['Value 500 000 - 700 000 EUR', 'Excluding VAT'],
+        ['Lowest offer  and highest offer 105 000 000,00 CZK', 'Excluding VAT'],
+        ['Lowest offer 20,02 and highest offer  EUR'],
+        ['Lowest offer 319 000 and highest offer  EUR', 'Including VAT. VAT rate (%) 16'],
+        ['Lowest offer 8 358 000 and highest offer  EUR', 'Excluding VAT'],
+        ['Lowest offer 196 350,90 and highest offer  EUR', 'Including VAT. VAT rate (%) 19'],
+        ['Lowest offer Cost per day after 60 Days: 1.10; One-Off Service Charge: 71.00; Inst. Rem & Maint cost to GMP(Normal Hours/Out of hours); 106.00/106.00; Average Cost per Force (Normal Hours/Out of Hours): 119.14/148.14 and highest offer Cost per day after 60 Days: 0.50; One-Off Service Charge: 950.00; Inst. Rem & Maint cost to GMP(Normal Hours/Out of hours): 950.00/1100.00; Average Cost per Force (Normal Hours/Out of Hours): 950.00/1100.00 GBP', 'Excluding VAT'],
+        ['Lowest offer  and highest offer 8 000 000 EUR', 'Including VAT. VAT rate (%) 19'],
+        ['Lowest offer 612 053 and highest offer  EUR', 'Excluding VAT']
         )
 
     for sample in samples:
