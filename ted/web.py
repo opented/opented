055e98d693939038ac09841a3d4f071c9dd7f926
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-10 18:46:23 +0200

    Basic operations in web app.

--- ted/web.py
+++ ted/web.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
-from common import get_engine
+from flask import Flask, Markup, render_template
+from dataset.util import slug
+from common import get_engine, list_years, list_countries
 
 app = Flask(__name__)
 engine = get_engine()
 document = engine['document']
 awards = engine['awards']
+
+def data_link(pattern, **values):
+    vf = {'type': 'documents'}
+    for k, v in values.items():
+        vf[k] = slug(unicode(v))
+    doc_url = pattern % vf
+    vf['type'] = 'awards'
+    aw_url = pattern % vf
+    return Markup('<a href="/data/'+doc_url+'">All documents</a> &middot; <a href="/data/'+aw_url+'">Contract awards</a> (CSV)')
+
+@app.context_processor
+def set_template_globals():
+    return {
+        'data': data_link
+        }
 
 # Permuations to export: 
 #
@@ -19,15 +35,18 @@ awards = engine['awards']
 # * All contracts by year and top-level CPV
 # * All contracts by year and top-level CPV
 
+@app.route("/country/<code>.html")
+def country(code):
+    return render_template('country.html',
+            years=list_years())
 
-@app.route("/")
+@app.route("/index.html")
 def index():
-    years = document.distinct('year')
-    countries = document.distinct('country')
     return render_template('index.html',
-            years=years, countries=countries)
+            years=list_years(), countries=list_countries())
 
 if __name__ == "__main__":
+    app.config['DEBUG'] = True
     app.run()
 
 
