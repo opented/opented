344377fc6edefd637c458d9e557331c5087606a8
Friedrich Lindenberg <friedrich@pudo.org>
2014-04-17 11:34:49 +0200

    Basic controllers.

---
+++ opented/util.py
@@ -0,0 +1,80 @@
+import os
+from unicodecsv import writer
+from cStringIO import StringIO
+
+from flask import Markup
+from slugify import slugify
+from jinja2.filters import do_filesizeformat
+
+from opented.core import app
+
+
+def file_size(url):
+    try:
+        size = float(os.path.getsize(os.path.join(get_output_dir(), 'data', url)))
+    except OSError:
+        size = 0
+    for x in ['bytes','KB','MB','GB','TB']:
+        if size < 1024.0:
+            return "%3.1f %s" % (size, x)
+        size /= 1024.0
+
+
+def data_link(pattern, **values):
+    vf = {'type': 'documents'}
+    for k, v in values.items():
+        vf[k] = slugify(unicode(v))
+    doc_url = pattern % vf
+    doc_size = file_size(doc_url)
+    vf['type'] = 'awards'
+    aw_url = pattern % vf
+    aw_size = file_size(aw_url)
+    return Markup('<a href="/data/'+doc_url+'">All documents</a> (CSV, '+doc_size+') &middot; <a href="/data/'+aw_url+'">Contract awards</a> (CSV, '+aw_size+')')
+
+
+def csv_write_line(line):
+    sio = StringIO()
+    w = writer(sio, encoding='utf-8')
+    w.writerow(line)
+    return sio.getvalue()
+
+
+def stream_csv(q):
+    keys = None
+    for row in q:
+        if keys is None:
+            keys = row.keys()
+            yield csv_write_line(keys)
+        values = [row.get(k) for k in keys]
+        yield csv_write_line(values)
+
+
+@app.context_processor
+def set_template_globals():
+    return {
+        'data': data_link
+        }
+
+
+def group(number):
+    s = '%d' % number
+    groups = []
+    while s and s[-1].isdigit():
+        groups.append(s[-3:])
+        s = s[:-3]
+    return s + ','.join(reversed(groups))
+
+
+@app.template_filter('format_num')
+def format_num(num):
+    if num is None or (isinstance(num, basestring) and not len(num)):
+        return '-'
+    try:
+        return group(int(num))
+    except Exception, e:
+        raise
+        return '-'
+
+
+def get_output_dir():
+    return 'site/'
---
+++ opented/views.py
@@ -0,0 +1,73 @@
+import os
+from datetime import datetime
+
+from flask import render_template, Response, url_for
+from monnet.util import engine
+
+from opented.core import app
+from opented.queries import list_years, list_countries, list_full
+from opented.queries import documents_query, contracts_query
+from opented.util import get_output_dir, stream_csv
+
+
+@app.route("/data/ted-documents.csv")
+@app.route("/data/ted-documents-<year>.csv")
+@app.route("/data/<country>/ted-documents-<country_>.csv")
+@app.route("/data/<country>/ted-documents-<country_>-<year>.csv")
+def documents(year=None, country=None, country_=None):
+    #print ['documents', year, country]
+    return 'banana'
+    q = documents_query(year=year, country=country)
+    return Response(stream_csv(q), mimetype='text/csv')
+
+
+@app.route("/data/ted-contracts.csv")
+@app.route("/data/ted-contracts-<year>.csv")
+@app.route("/data/<country>/ted-contracts-<country_>.csv")
+@app.route("/data/<country>/ted-contracts-<country_>-<year>.csv")
+def contracts(year=None, country=None, country_=None):
+    #print ['contracts', year, country]
+    return 'banana'
+    q = contracts_query(year=year, country=country)
+    return Response(stream_csv(q), mimetype='text/csv')
+
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    countries = list_countries()
+    all_ = {
+        'iso_country': None,
+        'country_common': 'All countries',
+        'section': 'all',
+        'documents': sum([c.get('documents') for c in countries])
+    }
+    tables = [all_] + [dict(c) for c in countries]
+    full = list_full()
+    for table in tables:
+        cc = table['iso_country']
+        if 'section' not in table:
+            table['section'] = cc
+        args = {'country': cc, 'country_': cc}
+        table['rows'] = [{
+            'documents': table.get('documents'),
+            'year': 'All years',
+            'documents_url': url_for('documents', **args),
+            'contracts_url': url_for('contracts', **args),
+        }]
+        years = full if cc is not None else list_years()
+        for year in years:
+            if 'iso_country' in year and year['iso_country'] != cc:
+                continue
+            args['year'] = year.get('year')
+            year.update({
+                'documents_url': url_for('documents', **args),
+                'contracts_url': url_for('contracts', **args)
+                })
+            #print year
+            table['rows'].append(dict(year))
+        #print table
+    tables = sorted(tables, key=lambda t: t.get('documents'), reverse=True)
+
+    last_update = datetime.utcnow().strftime('%d.%m.%Y')
+    return render_template('index.html', tables=tables, last_update=last_update)
